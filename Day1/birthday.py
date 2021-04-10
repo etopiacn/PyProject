8024290f33c808d66dc9060324470ff03e0023f6
clive <etopiacn@outlook.com>
2021-04-10 18:04:44 +0800

    day1

---
+++ Day1/birthday.py
@@ -0,0 +1,10 @@
+#序列:字符串、列表、元组
+# 根据年份判断生肖
+chinese_zodiac = '猴鸡狗猪鼠牛虎兔龙蛇马羊'  #取余不是从鼠开始的，偷懒做法
+#print(list(chinese_zodiac))
+#print(chinese_zodiac[0])
+#print(chinese_zodiac[0:4])
+#print(chinese_zodiac[-1])
+year = 2018
+print(year%12)
+print(chinese_zodiac[year%12])
